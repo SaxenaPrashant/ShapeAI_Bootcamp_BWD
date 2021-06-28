15c99ffd0fc39304eb9361f2d25ead642c992d4a
SaxenaPrashant <34087506+SaxenaPrashant@users.noreply.github.com>
2021-06-29 01:21:55 +0530

    Update and rename Info.jsx to Note.jsx

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>A basic web development React Js Bootcamp</p>
+      <p>
+        during these 7 days, and everything was explained from the very basics
+        so that anyone with zero experience on programming can learn.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
