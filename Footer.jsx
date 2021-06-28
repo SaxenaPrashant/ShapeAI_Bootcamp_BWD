deec29124319cd29e6c6820c0f84f878f3ded28e
SaxenaPrashant <34087506+SaxenaPrashant@users.noreply.github.com>
2021-06-28 18:20:51 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <Footer>
+            <p>Copy right by ShapeAI @ {new Date().getDate()}</p>
+        </Footer>
+    );
+}
+
+export default Footer;
---
+++ index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from "./components/App";
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World</h1>
+    </div>,
+    document.getElementById("root")
+);
+
+// ReactDOM.render(<App />, document.getElementById("root"));
